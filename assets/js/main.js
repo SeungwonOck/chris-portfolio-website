418cf6344913966c35683501c1273a72b5f4a8dc
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-07-27 13:52:46 +1000

    Section Home, About, Services, Projects, Contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,40 @@ const linkAction = () => {
 }
 
 navLink.forEach((link) => link.addEventListener("click", linkAction))
+
+// Shadow Header
+const shadowHeader = () => {
+    const header = document.getElementById("header")
+    this.scrollY >= 50 ? header.classList.add("shadow-header")
+                        : header.classList.remove("shadow-header")
+}
+
+window.addEventListener("scroll", shadowHeader);
+
+// Email Js
+const contactForm = document.getElementById("contact-form");
+const contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm("service_efzj69r", "template_oatirfd", "#contact-form", "TDbEReCl-hJ2cXeSt")
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅"
+
+        // Remove message
+        setTimeout(() => {
+            contactMessage.textContent = ""
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset();
+    }, () => {
+        //Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌"
+    })
+}
+
+contactForm.addEventListener("submit", sendEmail);
