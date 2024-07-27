d40bb85d6d4b0d0f8ca4276031cf8374055cdff3
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-07-28 03:11:43 +1000

    Scroll Sections Active Link

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,35 @@ const sendEmail = (e) => {
 }
 
 contactForm.addEventListener("submit", sendEmail);
+
+// Show Scroll Up
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up")
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll")
+                        : scrollUp.classList.remove("show-scroll")
+}
+
+window.addEventListener("scroll", scrollUp);
+
+// Scroll Sections Active Link
+const sections = document.querySelectorAll("section[id]")
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute("id");
+        const sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    })
+}
+
+window.addEventListener("scroll", scrollActive);
