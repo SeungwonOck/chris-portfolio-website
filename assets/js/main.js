cb46a7ee3419e4916bc62c54e53d51abc2cc1b75
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-07-28 17:51:11 +1000

    Dark Mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,30 @@ const scrollActive = () => {
 }
 
 window.addEventListener("scroll", scrollActive);
+
+// Dark Light Theme
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme"
+const iconTheme = "ri-sun-line"
+
+//  Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
+    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
+}
+// Activate & Deactivate the them manually with the button
+themeButton.addEventListener("click", () => {
+    // Add or Remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    // Store the selected theme and icon in local storage
+    localStorage.setItem("selected-theme", getCurrentTheme())
+    localStorage.setItem("selected-icon", getCurrentIcon())
+})
